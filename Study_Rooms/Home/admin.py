409c5748e588a16229471f8bccb67cc45a06f372
Ahmed Rohail Awan <ahmedrohailawanwaheedyousaf@gmail.com>
2022-03-15 22:43:09 +0500

    Delete Study_Rooms directory

--- Study_Rooms/Home/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from Home.models import Room,Topic,Message
-from Home.models import User
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Room)
-admin.site.register(Topic)
-admin.site.register(Message)
